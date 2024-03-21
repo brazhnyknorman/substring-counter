5b927ebabc018000566b00666f0cec2c42bb001f
Norman <164359827+brazhnyknorman@users.noreply.github.com>
2024-03-21 11:41:08 -0400

    Update substring_counter.rb
    Removed elsif statement that did nothing

--- substring_counter.rb
+++ substring_counter.rb
@@ -4,11 +4,9 @@ def substrings(words, dictionary)
   substring_count = {}
   dictionary.each_with_index do |substring, index|
     if words.downcase.include?(substring)
-      if substring_count.dig(substring) == nil # Create key if nonexistent
+      if substring_count.dig(substring) == nil # Create key is nonexistent
         # Sets substring count to the number of occurences of <substring>
         substring_count[substring] = words.downcase.scan(substring).count
-      else 
-        substring_count[substring] += 1        # Update key if existent
       end
     end
   end
